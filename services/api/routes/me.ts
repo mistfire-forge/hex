543f64d826e16726f68387f0e763c6d96297a219
Michael Liu <michael@triangularcube.com>
2021-04-01 01:15:29 -0700

    Add authorizer

--- services/api/routes/me.ts
+++ services/api/routes/me.ts
@@ -1,12 +1,19 @@
 import * as awsx from '@pulumi/awsx'
 import { createLambda } from '../utils/createLambda'
+import { authorizer } from '../utils/authorizer'
 
 const handler = async (
   event: awsx.apigateway.Request
 ): Promise<awsx.apigateway.Response> => {
   return {
     statusCode: 200,
-    body: event.requestContext.stage,
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      stage: event.requestContext.stage,
+      headers: event.headers,
+    }),
   }
 }
 
@@ -14,4 +21,5 @@ export const me: awsx.apigateway.Route = {
   path: '/me',
   method: 'GET',
   eventHandler: createLambda('me', handler),
+  authorizers: authorizer,
 }
